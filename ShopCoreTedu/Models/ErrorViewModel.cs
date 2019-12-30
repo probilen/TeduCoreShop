2ce4ab7ab942d529060fe6aab60852d3d64c3b1d
Khoa <trankhoa0702@gmail.com>
2019-12-30 22:17:02 +0700

    Add Project to master by TranKhoa

---
+++ ShopCoreTedu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopCoreTedu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
